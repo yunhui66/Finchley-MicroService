3149ba4ba037501467091241b005188cf1bd6417
yunhui <yunhui_root@163.com>
2020-01-21 17:48:24 +0800

    Initial commit

---
+++ finchley-microservice-eureka/src/main/java/com/microservice/finchleymicroserviceeureka/FinchleyMicroserviceEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.finchleymicroserviceeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinchleyMicroserviceEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinchleyMicroserviceEurekaApplication.class, args);
+    }
+
+}
---
+++ finchley-microservice-eureka/src/test/java/com/microservice/finchleymicroserviceeureka/FinchleyMicroserviceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.finchleymicroserviceeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinchleyMicroserviceEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
